2d5af4912178008bfac17f03ca55d6b900a9ece5
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2022-10-18 20:36:08 +0900

    Initial Commit

---
+++ ConvenienceToolApp/ConvenienceToolAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvenienceToolAppApp.swift
+//  ConvenienceToolApp
+//
+//  Created by 渡邊魁優 on 2022/10/18.
+//
+
+import SwiftUI
+
+@main
+struct ConvenienceToolAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
